7135bc34aba7798d9a60bc02296617dbc42ac110
Christoph Martenka <christoph.martenka@yahoo.de>
2024-12-03 10:14:03 +0100

    :sparkles: Add Day 1 Solution

---
+++ advent-of-code-2024/day1/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace advent.of.code.day1;
+
+internal static class Solution {
+    internal static int Task1(StreamReader reader) {
+        ReadLists(reader, out var left, out var right);
+
+        left.Sort();
+        right.Sort();
+
+        return left.Select((value, index) => Math.Abs(value - right[index]))
+            .Sum();
+    }
+
+    internal static int Task2(StreamReader reader) {
+        ReadLists(reader, out var left, out var right);
+
+        return (
+            from leftNumber in left
+            let count = right.Count(number => number == leftNumber)
+            select leftNumber * count
+        ).Sum();
+    }
+
+    private static void ReadLists(StreamReader reader, out List<int> left, out List<int> right) {
+        left = [];
+        right = [];
+
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine() ?? throw new Exception();
+
+            var match = Regex.Match(line, @"(?<left>\d+)\s+(?<right>\d+)");
+            left.Add(int.Parse(match.Groups["left"].Value));
+            right.Add(int.Parse(match.Groups["right"].Value));
+        }
+    }
+}
---
+++ advent-of-code-2024/day1/Test.cs
@@ -0,0 +1,22 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day1;
+
+[TestFixture]
+internal class Test {
+
+    private const int Day = 1;
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 1 should be successful with test input", ExpectedResult = 11)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 1 should be successful with real input", ExpectedResult = 1110981)]
+    public int Task1_Test(int day, string fileName) {
+        return Solution.Task1(FileReader.GetFileForDay(day, fileName));
+    }
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 2 should be successful with test input", ExpectedResult = 31)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 2 should be successful with real input", ExpectedResult = 24869388)]
+    public int Task2_Test(int day, string fileName) {
+        return Solution.Task2(FileReader.GetFileForDay(day, fileName));
+    }
+}
