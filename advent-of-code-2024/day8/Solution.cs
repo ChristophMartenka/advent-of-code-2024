624c487d884399f39ff823eb43a45983974917cd
Christoph Martenka <christoph.martenka@yahoo.de>
2024-12-08 09:30:46 +0100

    :sparkles: Add Day 8 Solution

---
+++ advent-of-code-2024/day8/Solution.cs
@@ -0,0 +1,84 @@
+﻿namespace advent.of.code.day8;
+
+internal static class Solution {
+    internal static int Task1(StreamReader reader) {
+        var map = ReadMap(reader);
+        var antennaLocationsByFrequency = ReadAntennaLocations(map);
+        return FindAntinodes(antennaLocationsByFrequency, map.Count, map[0].Length, false).Count;
+    }
+
+    internal static int Task2(StreamReader reader) {
+        var map = ReadMap(reader);
+        var antennaLocationsByFrequency = ReadAntennaLocations(map);
+        var antinodeLocations = FindAntinodes(antennaLocationsByFrequency, map.Count, map[0].Length, true);
+
+        // Add all antenna locations to the antinode locations
+        antinodeLocations.UnionWith(antennaLocationsByFrequency.Values.SelectMany(antennas => antennas));
+
+        return antinodeLocations.Count;
+    }
+
+    private static List<char[]> ReadMap(StreamReader reader) {
+        var map = new List<char[]>();
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine() ?? throw new Exception();
+            map.Add(line.ToCharArray());
+        }
+
+        return map;
+    }
+
+    private static Dictionary<char, List<Point>> ReadAntennaLocations(List<char[]> map) {
+        var frequencies = new Dictionary<char, List<Point>>();
+        for (var y = 0; y < map.Count; y++) {
+            for (var x = 0; x < map[y].Length; x++) {
+                var frequency = map[y][x];
+                if (frequency == '.') continue;
+                if (!frequencies.ContainsKey(frequency)) {
+                    frequencies.Add(frequency, []);
+                }
+
+                frequencies[frequency].Add(new Point(y, x));
+            }
+        }
+
+        return frequencies;
+    }
+
+    private record Point(int Y, int X) {
+        public static Point operator +(Point a, Point b) => new(a.Y + b.Y, a.X + b.X);
+        public static Point operator -(Point a, Point b) => new(a.Y - b.Y, a.X - b.X);
+        public static Point operator *(Point a, int val) => new(a.Y * val, a.X * val);
+    }
+
+    private static HashSet<Point> FindAntinodes(
+        Dictionary<char, List<Point>> antennaLocationsByFrequency,
+        int height,
+        int width,
+        bool resonant
+    ) {
+        var locations = new HashSet<Point>();
+        foreach (var antennas in antennaLocationsByFrequency.Values) {
+            for (var i = 0; i < antennas.Count; i++) {
+                var antennaA = antennas[i];
+                for (var j = i + 1; j < antennas.Count; j++) {
+                    var antennaB = antennas[j];
+                    var direction = antennaA - antennaB;
+                    locations.UnionWith(GetAntinodes(antennaA, direction, height, width, resonant));
+                    locations.UnionWith(GetAntinodes(antennaB, direction * -1, height, width, resonant));
+                }
+            }
+        }
+
+        return locations;
+    }
+
+    private static IEnumerable<Point> GetAntinodes(Point start, Point direction, int height, int width, bool resonant) {
+        var antinode = start + direction;
+        while (antinode.Y >= 0 && antinode.Y < height && antinode.X >= 0 && antinode.X < width) {
+            yield return antinode;
+            if (!resonant) break;
+            antinode += direction;
+        }
+    }
+}
---
+++ advent-of-code-2024/day8/Test.cs
@@ -0,0 +1,22 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day8;
+
+[TestFixture]
+internal class Test {
+
+    private const int Day = 8;
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 1 should be successful with test input", ExpectedResult = 14)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 1 should be successful with real input", ExpectedResult = 367)]
+    public int Task1_Test(int day, string fileName) {
+        return Solution.Task1(FileReader.GetFileForDay(day, fileName));
+    }
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 2 should be successful with test input", ExpectedResult = 34)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 2 should be successful with real input", ExpectedResult = 1285)]
+    public int Task2_Test(int day, string fileName) {
+        return Solution.Task2(FileReader.GetFileForDay(day, fileName));
+    }
+}
