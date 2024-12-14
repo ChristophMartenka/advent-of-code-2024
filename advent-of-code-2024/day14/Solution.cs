3033011bdc2d65a38f750bfac954ada4086e9ef6
Christoph Martenka <christoph.martenka@yahoo.de>
2024-12-14 10:45:35 +0100

    :sparkles: Add Day 14 Solution

---
+++ advent-of-code-2024/day14/Solution.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.RegularExpressions;
+
+namespace advent.of.code.day14;
+
+internal static class Solution {
+    internal static int Task1(StreamReader reader, int width, int height) {
+        var robots = ReadRobots(reader);
+
+        for (var step = 0; step < 100; step++) {
+            PerformStep(ref robots, width, height);
+        }
+
+        return robots
+            .Where(robot => !(robot.Pos.Y == height / 2 || robot.Pos.X == width / 2))
+            .GroupBy(robot => (robot.Pos.Y < height / 2 ? 0 : 1) + (robot.Pos.X < width / 2 ? 0 : 2))
+            .Aggregate(1, (current, quadrant) => current * quadrant.Count());
+    }
+
+    internal static int Task2(StreamReader reader, int width, int height) {
+        var robots = ReadRobots(reader);
+
+        var step = 0;
+        do {
+            step++;
+            PerformStep(ref robots, width, height);
+        } while (robots.GroupBy(r => r.Pos).Any(g => g.Count() > 1));
+
+        // Print step displaying the tree
+        // for (var y = 0; y < height; y++) {
+        //     for (var x = 0; x < width; x++) {
+        //         var count = robots.Count(r => r.X == x && r.Y == y);
+        //         Console.Write(count > 0 ? count : ".");
+        //     }
+        //     Console.WriteLine();
+        // }
+
+        return step;
+    }
+
+    private record Robot(Vector2 Pos, Vector2 Speed);
+
+    private static List<Robot> ReadRobots(StreamReader reader) {
+        var robots = new List<Robot>();
+
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine() ?? throw new Exception();
+            var match = Regex.Match(line, @"^p=(?<X>\d+),(?<Y>\d+)\sv=(?<XSpeed>-?\d+),(?<YSpeed>-?\d+)$");
+
+            robots.Add(new Robot(
+                new Vector2(int.Parse(match.Groups["X"].Value), int.Parse(match.Groups["Y"].Value)),
+                new Vector2(int.Parse(match.Groups["XSpeed"].Value), int.Parse(match.Groups["YSpeed"].Value))
+            ));
+        }
+
+        return robots;
+    }
+
+    private static void PerformStep(ref List<Robot> robots, int width, int height) {
+        for (var i = 0; i < robots.Count; i++) {
+            var robot = robots[i];
+            robots[i] = robot with { Pos = (robot.Pos + robot.Speed).WithinBounds(width, height) };
+        }
+    }
+
+    private record Vector2(int X, int Y) {
+        public static Vector2 operator +(Vector2 a, Vector2 b) => new(a.X + b.X, a.Y + b.Y);
+
+        internal Vector2 WithinBounds(int maxX, int maxY) => new(
+            X < 0 ? X + maxX : X >= maxX ? X - maxX : X,
+            Y < 0 ? Y + maxY : Y >= maxY ? Y - maxY : Y
+        );
+    }
+}
---
+++ advent-of-code-2024/day14/Test.cs
@@ -0,0 +1,23 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day14;
+
+[TestFixture]
+internal class Test {
+
+    private const int Day = 14;
+
+    [TestCase(Day, "testInput.txt", 11, 7, TestName = "Day {0} Part 1 should be successful with test input", ExpectedResult = 12)]
+    [TestCase(Day, "input.txt", 101, 103, TestName = "Day {0} Part 1 should be successful with real input", ExpectedResult = 229069152)]
+    public int Task1_Test(int day, string fileName, int width, int height) {
+        return Solution.Task1(FileReader.GetFileForDay(day, fileName), width, height);
+    }
+
+    // Test input is not applicable for this task
+    // [TestCase(Day, "testInput.txt", 11, 7, TestName = "Day {0} Part 2 should be successful with test input", ExpectedResult = 0)]
+    [TestCase(Day, "input.txt", 101, 103, TestName = "Day {0} Part 2 should be successful with real input", ExpectedResult = 7383)]
+    public int Task2_Test(int day, string fileName, int width, int height) {
+        return Solution.Task2(FileReader.GetFileForDay(day, fileName), width, height);
+    }
+}
