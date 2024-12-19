53134d9cfe2a90a2cd9f8b91e29279e1db425577
Christoph Martenka <christoph.martenka@yahoo.de>
2024-12-19 10:53:30 +0100

    :sparkles: Add Day 19 Solution

---
+++ advent-of-code-2024/day19/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace advent.of.code.day19;
+
+internal static class Solution {
+    internal static int Task1(StreamReader reader) {
+        ReadInput(reader, out var towels, out var patterns);
+        return patterns.AsParallel()
+            .Count(pattern => CountPossibleCombinations(towels, pattern, new Dictionary<string, long>()) > 0);
+    }
+
+    internal static long Task2(StreamReader reader) {
+        ReadInput(reader, out var towels, out var patterns);
+        return patterns.AsParallel()
+            .Select(pattern => CountPossibleCombinations(towels, pattern, new Dictionary<string, long>()))
+            .Sum();
+    }
+
+    private static void ReadInput(StreamReader reader, out List<string> towels, out List<string> patterns) {
+        var line = reader.ReadLine() ?? throw new Exception();
+        towels = line.Split(", ").ToList();
+
+        reader.ReadLine();
+
+        patterns = [];
+        while (!reader.EndOfStream) {
+            patterns.Add(reader.ReadLine() ?? throw new Exception());
+        }
+    }
+
+    private static long CountPossibleCombinations(List<string> towels, string pattern, Dictionary<string, long> cache) {
+        if (cache.TryGetValue(pattern, out var cachedCombinations)) {
+            return cachedCombinations;
+        }
+
+        var combinations = 0L;
+
+        foreach (var towel in towels) {
+            if (towel.Length < pattern.Length && pattern[..towel.Length] == towel) {
+                combinations += CountPossibleCombinations(towels, pattern[towel.Length..], cache);
+            }
+            else if (towel == pattern) {
+                combinations++;
+            }
+        }
+
+        cache[pattern] = combinations;
+        return combinations;
+    }
+}
---
+++ advent-of-code-2024/day19/Test.cs
@@ -0,0 +1,22 @@
+﻿using advent.of.code.util;
+using NUnit.Framework;
+
+namespace advent.of.code.day19;
+
+[TestFixture]
+internal class Test {
+
+    private const int Day = 19;
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 1 should be successful with test input", ExpectedResult = 6)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 1 should be successful with real input", ExpectedResult = 290)]
+    public int Task1_Test(int day, string fileName) {
+        return Solution.Task1(FileReader.GetFileForDay(day, fileName));
+    }
+
+    [TestCase(Day, "testInput.txt", TestName = "Day {0} Part 2 should be successful with test input", ExpectedResult = 16)]
+    [TestCase(Day, "input.txt", TestName = "Day {0} Part 2 should be successful with real input", ExpectedResult = 712058625427487)]
+    public long Task2_Test(int day, string fileName) {
+        return Solution.Task2(FileReader.GetFileForDay(day, fileName));
+    }
+}
